6f0ca91305930f1e8c3ae628c06bbe11ebc18888
RohimChou <rohim.chou@gmail.com>
2022-09-12 22:56:30 +0800

    001-HelloWorld bundle css and devServer

--- 001-HelloWorld/webpack.config.js
+++ 001-HelloWorld/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 console.log("The current path is:", path.resolve(__dirname));
 
@@ -7,13 +8,23 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: "/dist/",
   },
+  plugins: [
+    new MiniCssExtractPlugin() // default will be main.css
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
+  },
+  devServer: {
+      static: {
+          directory: path.join(__dirname),
+      },
+      port: 8080,
   },
 };
