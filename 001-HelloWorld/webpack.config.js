2eff90a0c204d5bec3a274dff6c033ec5e96dc92
RohimChou <rohim.chou@gmail.com>
2022-09-12 23:44:19 +0800

    001/plugin/CopyWebpackPlugin

--- 001-HelloWorld/webpack.config.js
+++ 001-HelloWorld/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-console.log("The current path is:", path.resolve(__dirname));
+console.log("The current path is:", path.resolve(__dirname, 'src'));
 
 module.exports = {
   entry: './src/index.js',
@@ -11,7 +12,13 @@ module.exports = {
     publicPath: "/dist/",
   },
   plugins: [
-    new MiniCssExtractPlugin() // default will be main.css
+    new MiniCssExtractPlugin(), // default will be main.css
+    new CopyWebpackPlugin({
+      patterns:[{
+        from: path.resolve(__dirname, 'src/aaa'), 
+        to: path.resolve(__dirname, 'dist/aaa')
+      }]
+    }),
   ],
   module: {
     rules: [
