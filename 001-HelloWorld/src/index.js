8186cfbde8e4a7f7cda9ba6adab6a4e72bec05ff
RohimChou <rohim.chou@gmail.com>
2022-09-12 21:38:17 +0800

    001-HelloWorld include js and css

--- 001-HelloWorld/src/index.js
+++ 001-HelloWorld/src/index.js
@@ -1,1 +1,7 @@
-alert("Hello World");
+import ut from './utils';
+import './master.css';
+
+const a = 10;
+const b = ut.add(a, 5);
+const c = ut.times2(a);
+alert(`b: ${b}, c: ${c}`);
---
+++ 001-HelloWorld/src/utils.js
@@ -0,0 +1,8 @@
+export default {
+    add: function(a, b) {
+        return a + b;
+    },
+    times2(a) {
+        return a * 2;
+    }
+}
